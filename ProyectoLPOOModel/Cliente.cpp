34f64489bcef586d6dc66ad899082aec5b058c99
David <a20212557@pucp.edu.pe>
2023-09-21 13:15:14 -0500

    actualicé el model

--- ProyectoLPOOModel/Cliente.cpp
+++
@@ -1,9 +0,0 @@
-#include "Cliente.h"
-
-Cliente::Cliente() {
-
-}
-Cliente::Cliente(String^ ListaDecompras, double PuntosBonus) {
-	this->ListaDecompras = ListaDecompras;
-	this->PuntosBonus = PuntosBonus;
-}
